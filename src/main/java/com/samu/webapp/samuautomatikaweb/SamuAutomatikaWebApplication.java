605867b4ba5ff8f79099893c83d47421646becc3
DanielAdamZoltan <69815516+DanielAdamZoltan@users.noreply.github.com>
2021-09-07 11:47:13 +0200

    version 1

---
+++ src/main/java/com/samu/webapp/samuautomatikaweb/SamuAutomatikaWebApplication.java
@@ -0,0 +1,13 @@
+package com.samu.webapp.samuautomatikaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuAutomatikaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamuAutomatikaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samu/webapp/samuautomatikaweb/SamuAutomatikaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samu.webapp.samuautomatikaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuAutomatikaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
