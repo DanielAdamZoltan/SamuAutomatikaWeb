6845dee02c704d0db6634ad60976b04290e7065a
DanielAdamZoltan <69815516+DanielAdamZoltan@users.noreply.github.com>
2021-09-10 17:47:07 +0200

    all other site added, nav menu all the sides

--- src/main/java/com/samu/webapp/samuautomatikaweb/controller/HomeController.java
+++ src/main/java/com/samu/webapp/samuautomatikaweb/controller/HomeController.java
@@ -12,11 +12,51 @@ public class HomeController {
         return "../static/webpages/index";
     }
 
-    @GetMapping("/jobs")
+    @GetMapping("/index/")
+    public String viewHomePageIndex()
+    {
+        return "../static/webpages/index";
+    }
+
+    @GetMapping("/jobs/")
     public String viewJobsPage()
     {
         return "../static/webpages/jobs";
     }
+
+    @GetMapping("/products/")
+    public String viewProductsPage() {return "../static/webpages/products";}
+
+    @GetMapping("/building-automation/")
+    public String viewBuildingAutomationPage() {return "../static/webpages/building-automation";}
+
+    @GetMapping("/energy-management/")
+    public String viewEnergyManagementPage() {return "../static/webpages/energy-management";}
+
+    @GetMapping("/fan-coil/")
+    public String viewFanCoilPage() {return "../static/webpages/fan-coil";}
+
+    @GetMapping("/air-treatment/")
+    public String viewAirTreatmentPage() {return "../static/webpages/air-treatment";}
+
+    @GetMapping("/thermostat/")
+    public String viewThermostatPage() {return "../static/webpages/thermostat";}
+
+    @GetMapping("/heat-central/")
+    public String viewHeatCentralPage() {return "../static/webpages/heat-central";}
+
+    @GetMapping("/references/")
+    public String viewReferencesPage() {return "../static/webpages/references";}
+
+    @GetMapping("/contact/")
+    public String viewContactPage() {return "../static/webpages/contact";}
+
+    @GetMapping("/adatvedelem/")
+    public String viewDataProtectionPage() {return "../static/webpages/adatvedelem";}
+
+
+
+
 
 
 }
--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,5 +1,3 @@
-
-
 var language;
 function getLanguage(lang) {
     $.ajax({
@@ -12,6 +10,7 @@ function getLanguage(lang) {
 }
 
 function setLanguage() {
+    //menü
     $('#home').html(language["home"]);
     $('#scada').text(language["scada"]);
     $('#jobs').text(language["jobs"]);
@@ -25,9 +24,48 @@ function setLanguage() {
     $('#heat-central').text(language["heat-central"]);
     $('#references').text(language["references"]);
     $('#contact').text(language["contact"]);
-
+    //nyelv választó
     $('#languageSelector').text(language["language"]);
+
+    //
+    //$('#home-page-title').text(language["home-page-title"]);
+
 }
+
+
+document.addEventListener("DOMContentLoaded", function(){
+// make it as accordion for smaller screens
+    if (window.innerWidth > 992) {
+
+        document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+
+            everyitem.addEventListener('mouseover', function(e){
+
+                let el_link = this.querySelector('a[data-bs-toggle]');
+
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                    nextEl.classList.add('show');
+                }
+
+            });
+            everyitem.addEventListener('mouseleave', function(e){
+                let el_link = this.querySelector('a[data-bs-toggle]');
+
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.remove('show');
+                    nextEl.classList.remove('show');
+                }
+
+
+            })
+        });
+
+    }
+// end if innerWidth
+});
 
 $(document).ready(function(){
     getLanguage('hungarian');
